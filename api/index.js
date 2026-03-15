45b51c42d3eddb26ccc1f5db09af4f7e1f42ac70
Ankitha <avankitha.10a@gmail.com>
2026-03-15 21:36:44 +0530

    fix vercel serverless api

--- api/index.js
+++ api/index.js
@@ -1,33 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("../connect/connect");
+const { router } = require("../routes/route");
 
-const express = require('express')
 const app = express();
 
-const cors = require('cors')
-app.use(cors())
+app.use(cors());
+app.use(express.json());
+
+connectDB(process.env.MONGO_URI);
 
 app.get("/", (req, res) => {
-  res.send("Task Manager API running");
+  res.send("Task Manager API Running");
 });
 
-const {task} = require('../routes/route');
-require('../connect/connect')
+app.use("/api/v1", router);
 
-app.use('/',task)
-
-require('dotenv').config()
-
-const port = process.env.PORT
-
-const connectDB = require('../connect/connect')
-
-const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port, () => {
-        console.log(`listening at port ${port}`)
-})
-    } catch (error) {
-        console.log("DB is not connected")
-    }
-}
-start()
+module.exports = app;
